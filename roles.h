0c6a374f8cccf23b640353e1b2075f5e7cc54919
Muqtada mushtaq abbas <115376448+cv1awaw@users.noreply.github.com>
2024-12-19 18:45:03 +0300

    Create roles.h

---
+++ roles.h
@@ -0,0 +1,43 @@
+// roles.h
+
+#ifndef ROLES_H
+#define ROLES_H
+
+#include <unordered_set>
+
+// Define user IDs for each role
+const std::unordered_set<int64_t> WRITER_IDS = {
+    7491629866, 7030185248, 1414370194, 585812065,
+    6969704654, 1162719050, 6377619159, 935602501,
+    6702007291, 6554665
+};
+
+const std::unordered_set<int64_t> MCQS_TEAM_IDS = {
+    6690281336, 11111, 65456664
+};
+
+const std::unordered_set<int64_t> CHECKER_TEAM_IDS = {
+    591536381, 1414370194, 65556765, 523278185, 933155332
+};
+
+const std::unordered_set<int64_t> WORD_TEAM_IDS = {
+    7491629866, 758645645, 65557544
+};
+
+const std::unordered_set<int64_t> DESIGN_TEAM_IDS = {
+    7030185248, 7354567, 65445
+};
+
+const std::unordered_set<int64_t> KING_TEAM_IDS = {
+    56676, 73545678, 6898449897
+};
+
+const std::unordered_set<int64_t> TARA_TEAM_IDS = {
+    137745730, 6177929931, 333135898, 6898449897
+};
+
+const std::unordered_set<int64_t> MIND_MAP_FORM_CREATOR_IDS = {
+    1703780092, 7655565
+};
+
+#endif // ROLES_H
